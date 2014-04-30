278f3a6e28a7cffce0a413dbcade45f03b154b01
Matt Soltani <solt@dr.dk>
2014-04-30 22:20:51 +0200

    Testing js task

--- js/require.config.js
+++ js/require.config.js
@@ -2,25 +2,25 @@ require.config({
 	baseUrl: ((location.hostname === "beta.dr.dk" || location.hostname === "localhost") ? "//www.dr.dk" : "") + "/assets/js/",
 	paths: {
 		// first party        
-		//"dr-media-player-factory": "005/dr/widgets/dr-widget-media",
-		//"dr-media-gemius-implementation": "005/dr/widgets/dr-widget-media",
-		//"dr-media-gemius-implementation-test": "005/dr/widgets/dr-widget-media",
-		//"dr-media-conviva-implementation": "005/dr/widgets/dr-widget-media",
-		//"dr-media-sola-implementation": "005/dr/widgets/dr-widget-media",
-		//"dr-media-springstreams-implementation": "005/dr/widgets/dr-widget-media",
-		//"dr-media-hash-implementation": "005/dr/widgets/dr-widget-media",
-		//"dr-widget-video-player-factory": "005/dr/widgets/dr-widget-media",
-		//"dr-widget-video-player": "005/dr/widgets/dr-widget-media",
-		//"dr-widget-video-playlist": "005/dr/widgets/dr-widget-media",
-		//"dr-widget-audio-player": "005/dr/widgets/dr-widget-media",
-		//"dr-widget-audio-playlist": "005/dr/widgets/dr-widget-media",
-		//"dr-widget-media-playlist": "005/dr/widgets/dr-widget-media",
-		//"dr-media-audio-player": "005/dr/widgets/dr-widget-media",
-		//"dr-media-html5-audio-player": "005/dr/widgets/dr-widget-media/Html5AudioPlayer",
-		//"dr-media-flash-audio-player": "005/dr/widgets/dr-widget-media/FlashAudioPlayer",
-
+		//"dr-media-player-factory": "modules/dr-widget-media",
+		//"dr-media-gemius-implementation": "modules/dr-widget-media",
+		//"dr-media-gemius-implementation-test": "modules/dr-widget-media",
+		//"dr-media-conviva-implementation": "modules/dr-widget-media",
+		//"dr-media-sola-implementation": "modules/dr-widget-media",
+		//"dr-media-springstreams-implementation": "modules/dr-widget-media",
+		//"dr-media-hash-implementation": "modules/dr-widget-media",
+		//"dr-widget-video-player-factory": "modules/dr-widget-media",
+		//"dr-widget-video-player": "modules/dr-widget-media",
+		//"dr-widget-video-playlist": "modules/dr-widget-media",
+		//"dr-widget-audio-player": "modules/dr-widget-media",
+		//"dr-widget-audio-playlist": "modules/dr-widget-media",
+		//"dr-widget-media-playlist": "modules/dr-widget-media",
+		//"dr-media-audio-player": "modules/dr-widget-media",
+		//"dr-media-html5-audio-player": "modules/dr-widget-media/Html5AudioPlayer",
+		//"dr-media-flash-audio-player": "modules/dr-widget-media/FlashAudioPlayer",
 
 		// third party
-		//"jquery": "005/more"
+		"jquery": "third/jquery-1.11.0.js",
+		"modernizr": "third/modernizr.js"
 	},
 });
