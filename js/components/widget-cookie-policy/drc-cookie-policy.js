275d4a808c39a26f0c5dbdaa8e810ec2dbb86617
Jeppe Hasseriis     TU Web Applikationer <jehr@dr.dk>
2017-11-10 14:06:55 +0100

    Define drc-cookie-policy as CJS. Disable requirejs cookie-policy in core component.

--- js/components/widget-cookie-policy/drc-cookie-policy.js
+++ js/components/widget-cookie-policy/drc-cookie-policy.js
@@ -1,6 +1,2 @@
-(function (win, doc) {
-  win.define("cookie-policy", function() {
-    var cookiePolicyLoader = require('@dr/drc-global/cookie-policy');
-    cookiePolicyLoader.initialize();
-  });
-})(window, document);
+var cookiePolicyLoader = require('@dr/drc-global/cookie-policy');
+cookiePolicyLoader.initialize();
