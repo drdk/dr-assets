cde7a9b1daaf4496c6c25e85cf33bb964f29aa17
Jeppe Hasseriis     TU Web Applikationer <jehr@dr.dk>
2017-11-13 13:39:03 +0100

    Add rollup

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import less from 'rollup-plugin-less';
+// rollup.config.js
+export default {
+  input: 'js/components/widget-cookie-policy/drc-cookie-policy.js',
+  output: {
+    file: 'js/components/widget-cookie-policy/cookie-policy.js',
+    format: 'amd'
+  },
+  plugins: [
+    less({
+      insert: true,
+      output: 'js/components/widget-cookie-policy/cookie-policy.inject.css',
+      exclude: []
+    }),
+    resolve(),
+    commonjs({
+    })
+  ]
+};
